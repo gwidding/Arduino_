29189c5c43e96e620d9cd78c9e370071a1be30be
귀띵 <135992700+gwidding@users.noreply.github.com>
2023-08-13 23:18:27 +0900

    목숨rgb옵션, 틀렸을 경우 재실행
    반복문 통과 시 괜찮은지 확인 해야 함.

--- 프로젝트/memoryGame/memoryGame.ino
+++ 프로젝트/memoryGame/memoryGame.ino
@@ -1,4 +1,4 @@
-#define MAX 16
+#define MAX 9
 const int VRX = A0; //조이스틱
 const int VRY = A1;
 const int SW = A2; //스위치
@@ -8,11 +8,14 @@ const int LED_R = 11;
 const int LED_D = 12;
 const int LED_L = 13;
 
-int cnt, level;
-int answer[MAX], response[MAX];
+int input_cnt = 0, level = 1, lifeN = 2; //1레벨, 목숨 3개로 시작, 목숨 0되면 죽음
+// 목숨 표시 -> RGB led
+const int RED = A3;
+const int GREED = A2;
+const int BLUE = A1;
 
-const int pins[] = {2,3,4,5,6,7,8,9}; //세그먼트
-const int numberCode[11] = {B01111111, //dp
+const int pins[] = {2,3,4,5,6,7,8}; //세그먼트 연결 
+const int numberCode[10] = {//B01111111, //dp
                             B11000000, //0 dp/gfedcba
                             B11111001, //1
                             B10100100, //2
@@ -34,90 +37,114 @@ void setup() {
   for (int i = 0; i < 7; i++){ //세그먼트
     pinMode(pins[i], OUTPUT);
   }
+
+  pinMode(RED, OUTPUT);
+  pinMode(GREEN, OUTPUT);
+  pinMode(BLUE, OUTPUT);
+  digitalWrite(GREEN, HIGH);
 }
 
+
 void loop() {
-  for (int i = 0; i < 3; i++){ //led 4
-    for (int j = 0; j < 9; j++){
-      on(j, 100);
+  int answer[level], response[level];
+  for (int i = 0; i < 3; i++){ //led 4개
+    for (int j = 10; j < 14; j++){ //10,11,12,13번 핀
+      ledOn(j, 100);
     }
   }
-  randQuestion(level);
-  inputJoy(level);
-  check(answer, response);
+  randQuestion(level); //문제 난수 생성
+  inputJoy(level); //정답 입력
+  collectCheck(answer, response); //정답확인
+
   delay(500);
 }
 
-void on(int n, int m){
+
+
+
+void ledOn(int n, int m){
   digitalWrite(n, HIGH);
   delay(50);
   digitalWrite(n, LOW);
   delay(50);
 }
 
-void randQuestion(int n){
-  for (int i = 0; i < n; i++){
-    int seed = random(0,4); 
-    answer.push_back(seed);
+void randQuestion(int n){ //n = 레벨
+  for (int i = 0; i < n; i++){ //1레벨이면 하나
+    int seed = random(10,14); 
+    answer[i] = seed;
   }
-  for (int i =0; i < answer.size(); i++){ 
+  for (int i = 0; i < answer.size(); i++){ 
     Serial.println(answer[i]); //정답 내용 확인용
-    on(answer[i],300); //level에 따라 속도 빨라지게,,?300 -10*level
+    ledOn(answer[i],300); //추가할 옵션 : level에 따라 속도 빨라지게,,?300 -10*level
   }
 }
 
-void inputJoy(int n){ //조이스틱으로 입력
-  int x = analogRead(VRX);
-  int y = analogRead(VRY);
-  int z = digitalRead(SW);
-  Serial.print(x); //잠시 확인용
-  Serial.print("   ");
-  Serial.print(y);
-  Serial.print("   ");
-  Serial.println(z);
+void inputJoy(int n){ //조이스틱으로 입력, n = 레벨
+  while(input_cnt < n){
+    int x = analogRead(VRX);
+    int y = analogRead(VRY);
+    int z = digitalRead(SW);
+    Serial.print(x); //잠시 확인용
+    Serial.print("   ");
+    Serial.print(y);
+    Serial.print("   ");
+    Serial.println(z);
 
-  while(cnt < n){
-    if (y == 1023){
-      on(LED_U, 300);
-      response[cnt] = 0;
+    if (y == 1023){ //위
+      ledOn(LED_U, 300);
+      response[cnt] = 10;
     }
     else if (x == 1023){
-      on(LED_R, 300);
-      response[cnt] = 1;
+      ledOn(LED_R, 300);
+      response[cnt] = 11;
     }
     else if (y == 0){
-      on(LED_D, 300);
-      response[cnt] = 2;
+      ledOn(LED_D, 300);
+      response[cnt] = 12;
     }
     else if (x == 0){
-      on(LED_L, 300);
-      response[cnt] = 3;
+      ledOn(LED_L, 300);
+      response[cnt] = 13;
     }
-    cnt++;
+    input_cnt++;
   }
 }
 
-void check(&ans, &resp){
+void collectCheck(&ans, &resp){
   bool equalAns = true;
   for (int i = 0; i < ans.size(); i++){
-    if (ans[i] != resp[i]) {
-      score--;
-      showScore(score);
-      //했던 배열로 재실행~
+    if (ans[i] != resp[i]) { //틀리면
+      lifeN--;
+      if(lifeN == 1){
+        digitalWrite(RED, LOW);
+        digitalWrite(BLUE, HIGH);
+      }
+      else if (lifeN == 0){
+        digitalWrute(BLUE, LOW);
+        digitalWrite(RED, HIGH);
+        break; //맞나?
+      }
+
+      showScore(level);
+      //했던 배열로 재실행
+      for (int i = 0; i < answer.size(); i++){ 
+        Serial.println(answer[i]); //정답 내용 확인용
+        ledOn(answer[i],300);
+      }
+      inputJoy(level);
+      collectCheck(&ans, &resp);
     }
   }
-  else {
-    score++;
-    level++;
-    showScore(level);
-  }
+  level++;
+  showScore(level); 
 }
 
-void showScore(int num){
-  int k = num++;
-  boolean value;
-  for (int bitP = 0; bitP < 8; bitP++){
-    value = bitRead(numberCode[k], bitP);
-    digitalWrite(pins[bitP], value); //해당하는 점수 세그먼트 출력
+void showScore(int num){ //num = 레벨
+  int k = num;
+  boolean segmentBit;
+  for (int bitP = 0; bitP < 8; bitP++){ //세그먼트
+    segmentBit = bitRead(numberCode[k], bitP); //해당 레벨 세그먼트 패턴 가져와서 넣음
+    digitalWrite(pins[bitP], segmentBit); //해당하는 세그먼트 led 켜기
   }
 }
